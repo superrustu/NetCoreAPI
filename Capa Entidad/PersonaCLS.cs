639247b1943a8f22d2fd8ea4bcd9a937354c1ec7
superrustu <36963633+superrustu@users.noreply.github.com>
2023-04-26 10:52:26 -0500

    creacion de proyecto

---
+++ Capa Entidad/PersonaCLS.cs
@@ -0,0 +1,9 @@
+﻿namespace Capa_Entidad;
+public class PersonaCLS
+{
+    public int idpersona { get; set; }
+    public string nombrecompleto { get; set; }
+    public string correo { get; set; }
+    public string fechanacimientocadena { get; set; }
+}
+
