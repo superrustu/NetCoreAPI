912ab5d86b2f3feec83eed5808a63b8d1eb99c21
richardvera <richard.vera@outlook.com>
2023-05-01 20:48:29 -0500

    test

--- Capa Entidad/PersonaCLS.cs
+++ Capa Entidad/PersonaCLS.cs
@@ -2,8 +2,19 @@
 public class PersonaCLS
 {
     public int idpersona { get; set; }
-    public string nombrecompleto { get; set; }
-    public string correo { get; set; }
-    public string fechanacimientocadena { get; set; }
+    public string nombrecompleto { get; set; } = "";
+    public string correo { get; set; } = "";
+    public string fechanacimientocadena { get; set; } = "";
+
+    //Defino campos de tabla
+    public string nombre { get; set; } = "";
+    public string appaterno { get; set; } = "";
+    public string apmaterno { get; set; } = "";
+    public DateTime fechanacimiento { get; set; }
+    public int idsexo { get; set; }
+
+    public string nombrearchivo { get; set; } = "";
+    public string fotocadena { get; set; } = "";
+    public byte[] archivo { get; set; }
 }
 
